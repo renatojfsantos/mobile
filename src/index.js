b453052aee85537ce787a2018d48b1058b434f0e
Renato Santos <renato_jfs@hotmail.com>
2020-10-02 22:45:10 -0300

    Esse commit é para o meu amigo Wagner

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+
+//Não possuem valor semântico (significado)
+//Não possuem estilização própria
+//Todos componentes possuem por padrão "display: flex"
+
+//View: div, footer, header, main, aside, section
+//Text: p, span, strong, h1, h2, h3
 
 export default function App() {
-  return <View />;
+  return (
+    <View style={styles.container}>
+    
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: '#7159c1',
+  }
+})
