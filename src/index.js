f40272ad7584c76a08498597accec3eb1906290c
Renato Santos <renato_jfs@hotmail.com>
2020-10-02 22:59:41 -0300

    View, Text, StatusBar with styles

--- src/index.js
+++ src/index.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
 
-//Não possuem valor semântico (significado)
-//Não possuem estilização própria
-//Todos componentes possuem por padrão "display: flex"
+// Não possuem valor semântico (significado)
+// Não possuem estilização própria
+// Todos componentes possuem por padrão "display: flex"
 
-//View: div, footer, header, main, aside, section
-//Text: p, span, strong, h1, h2, h3
+// View: div, footer, header, main, aside, section
+// Text: p, span, strong, h1, h2, h3
 
 export default function App() {
   return (
-    <View style={styles.container}>
-    
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello World</Text>
+      
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
     backgroundColor: '#7159c1',
-  }
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  title: {
+    color: '#FFF',
+    fontSize: 32,
+    fontWeight: "bold"
+  },
+
 })
