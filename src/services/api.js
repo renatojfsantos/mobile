12e860c99b038a6ac1bed1d80aa61ebb1dd49aae
Renato Santos <renato_jfs@hotmail.com>
2020-10-04 21:47:54 -0300

    Create baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,16 @@
 import axios from  'axios';
 
 const api = axios.create({
-  baseURL: ''
+  baseURL: '192.168.0.104',
 });
 
 export default api;
+
+/**
+ * ios com Emulador: localhost
+ * ios com dispositivo fisico: IP da maquina
+ * Android com Emulador: localhost executando (adb reverse)
+ * Android com Emulador: 10.0.2.2 (Android Studio)
+ * Android com Emulador: 10.0.3.2 (Genymotion)
+ * Android com dispositivo fisico: Ip da máquina
+ */
