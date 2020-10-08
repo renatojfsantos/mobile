398f8559aae3fb9b36d418dc0222e3830b99f78d
Renato Santos <renato_jfs@hotmail.com>
2020-10-07 23:10:08 -0300

    Usigin ScrollView for list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import { View, ScrollView, Text, StyleSheet, StatusBar } from 'react-native';
 
 import api from './services/api';
 
@@ -22,11 +22,11 @@ export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
-      <View style={styles.container}>
+      <ScrollView style={styles.container}>
         {projects.map(project => (
           <Text style={styles.project} key={project.id}>{project.title}</Text>
         ))}
-      </View>
+      </ScrollView>
     </>
   );
 }
@@ -35,8 +35,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7159c1',
-    justifyContent: "center",
-    alignItems: "center",
   },
 
   project: {
